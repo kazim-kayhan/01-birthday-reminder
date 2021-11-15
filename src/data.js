1b3c58193da1fcbe45f4e727c78eab63cc5d8c64
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-11-15 22:17:36 +0430

    birdthday reminder app

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Coder',
+    age: 29,
+    image:"images/coder.png",
+  },
+  {
+    id: 2,
+    name: 'Developer',
+    age: 32,
+    image:"images/developer.png"
+  },
+  {
+    id: 3,
+    name: 'Designer',
+    age: 36,
+    image:"images/designer.png"
+  },
+  {
+    id: 4,
+    name: 'Tester',
+    age: 34,
+    image:"images/tester.png"
+  },
+  {
+    id: 5,
+    name: 'Manager',
+    age: 29,
+    image:"images/manager.png"
+  },
+];
