0170cf15bb5cf6125ab1d021ff1a6c304b72f42e
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-12-19 16:27:54 +0430

    Add tailwind styles

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import List from './List';
 function App() {
   const [people, setPeople] = useState(data);
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} Birthday today</h3>
-        <List people={people}/>
-        <button onClick={()=>setPeople([])}>Clear All</button>
+    <main className="flex justify-center p-8 bg-cyan-200 min-h-screen">
+      <section className="flex flex-col bg-pink-400 px-auto rounded-xl p-8">
+        <h3 className="text-4xl font-bold p-4">
+          {people.length} Birthday Today
+        </h3>
+        <List people={people} />
+        <button
+          onClick={() => setPeople([])}
+          className="bg-green-400 hover:bg-green-500 p-2 mt-4 rounded-full text-xl font-bold"
+        >
+          Clear All
+        </button>
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -9,13 +9,14 @@ const List = ( { people } ) =>
         {
           const { id, name, age, image } = person;
           return (
-            <article key={ id } className="person">
-              <img src={ image } alt={ name } />
-              <div>
-                <h4>{ name }</h4>
-                <p>{ age } years</p>
+            <article key={id} className="flex p-2">
+              <img src={image} alt={name} className="h-16 w-16 rounded-full" />
+              <div className="flex flex-col px-4 justify-between">
+                <h4 className="font-bold">{name}</h4>
+                <p className="text-stone-700 font-semibold">{age} years</p>
               </div>
-            </article> )
+            </article>
+          );
         } )
       }
     </>
